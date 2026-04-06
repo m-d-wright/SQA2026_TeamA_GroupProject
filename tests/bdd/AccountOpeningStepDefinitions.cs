e6bde7a452eab87fd52d21531e1fb1a8ad297d45
OliverLaufenberg <oliver.laufenberg@Marquette.edu>
2026-04-06 16:13:06 -0500

    Bdd initial feature files and stepdefs (#4)
    * Added initial feature file and corresponding step definitions to match existing Account Opening tests
    Planning on same for transactions next
    
    * Added initial feature file and step definitions for transactions
    
    * Moved step definitions
    
    * Rename AccountOpeningStepDefinitions to AccountOpeningStepDefinitions.cs
    
    * Move TransactionsStepDefinitions to bdd directory
    
    * Moved feature file to bdd directory
    
    * Move Transactions.feature to bdd directory
    
    * Moved files to proper directory

---
+++ tests/bdd/AccountOpeningStepDefinitions.cs
@@ -0,0 +1,110 @@
+using Bank4Us.AccountOpening;
+using NSubstitute;
+using Reqnroll;
+using Reqnroll.Assist;
+using System;
+using static unit.AccountOpeningTests;
+
+namespace unit
+{
+    [Binding]
+    public class AccountOpeningStepDefinitions
+    {
+        private ScenarioContext _scenarioContext;
+
+        public AccountOpeningStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("I am an applicant for a new account with all valid details")]
+        public void GivenIAmAnApplicantForANewAccountWithAllValidDetails()
+        {
+            _scenarioContext["applicant"] = ApplicantFactory.CreateValid();
+            _scenarioContext["accountWorkflow"] =  new MockWorkflowFactory().CreateValid();
+            
+        }
+
+
+        [When("I submit the application")]
+        public void WhenISubmitTheApplication()
+        {
+            Applicant applicant = (Applicant)_scenarioContext["applicant"];
+            IAccountOpeningWorkflow accountWorkFlow = (IAccountOpeningWorkflow)_scenarioContext["accountWorkflow"];
+            AccountOpeningAutomation sut = new AccountOpeningAutomation(accountWorkFlow);
+
+            _scenarioContext["applicationResult"] = sut.Run(applicant);
+        }
+
+        [Then("I should see confirmation my account has been opened")]
+        public void ThenIShouldSeeConfirmationMyAccountHasBeenOpened()
+        {
+
+            ProcessResult processResult = (ProcessResult)_scenarioContext["applicationResult"];
+            Assert.NotNull(processResult);
+            Assert.Empty(processResult.Errors);
+            Assert.Equal(ApplicationStatus.Approved, processResult.Status);
+        }
+
+        [Given("I am an applicant for a new account with an invalid SSN")]
+        public void GivenIAmAnApplicantForANewAccountWithAnInvalidSSN()
+        {
+            Applicant applicant = new ApplicantBuilder().SetIDNumber("FAKE-SSN-NUM").Build();
+            ValidationError invalidIdError = new ValidationError("Invalid SSN format"); // Exact expected from reqs
+            IAccountOpeningWorkflow accountWorkFlow = new MockWorkflowFactory().CreateValid();
+            accountWorkFlow.ValidateIdentificationNumber(Arg.Any<Applicant>())
+                .Returns(new List<ValidationError> { invalidIdError });
+
+            _scenarioContext["accountWorkflow"] = accountWorkFlow;
+            _scenarioContext["applicant"] = applicant;
+            _scenarioContext["invalidIdError"] = invalidIdError;
+        }
+
+
+
+        [Then("I should see an error message about the invalid SSN")]
+        public void ThenIShouldSeeAnErrorMessageAboutTheInvalidSSN()
+        {
+            ProcessResult processResult = (ProcessResult)_scenarioContext["applicationResult"];
+            ValidationError invalidIdError = (ValidationError)_scenarioContext["invalidIdError"];
+
+            Assert.NotNull(processResult);
+            Assert.NotEmpty(processResult.Errors);
+            Assert.Equal(invalidIdError, processResult.Errors[0]);
+            Assert.Equal(ApplicationStatus.Incomplete, processResult.Status);
+
+        }
+
+        [Given("I am an applicant for a new account with initial deposit of {int}")]
+        public void GivenIAmAnApplicantForANewAccountWithInitialDepositOf(int depositAmount)
+        {
+            ValidationError invalidDepositError = new ValidationError("Invalid opening deposit");
+            IAccountOpeningWorkflow accountWorkFlow = new MockWorkflowFactory().CreateValid();
+
+            //I am aware that this is a janky bodge, but it's the only way to keep the structure consistent before we actually implement i
+            if (depositAmount < 200) 
+                accountWorkFlow.Process(Arg.Any<Applicant>())
+                .Returns(new ProcessResult(ApplicationStatus.Cancelled, new List<ValidationError> { invalidDepositError }));
+            
+            _scenarioContext["accountWorkflow"] = accountWorkFlow;
+            _scenarioContext["invalidDepositError"] = invalidDepositError;
+            _scenarioContext["applicant"] =
+                new ApplicantBuilder().SetOpeningDeposit(depositAmount).Build();
+
+        }
+
+        [Then("I should see an error message about insufficient initial deposit")]
+        public void ThenIShouldSeeAnErrorMessageAboutInsufficientInitialDeposit()
+        {
+            ProcessResult processResult = (ProcessResult)_scenarioContext["applicationResult"];
+            ValidationError invalidDepositError = (ValidationError)_scenarioContext["invalidDepositError"];
+
+            Assert.NotNull(processResult);
+            Assert.NotEmpty(processResult.Errors);
+            Assert.Equal(invalidDepositError, processResult.Errors[0]);
+            Assert.Equal(ApplicationStatus.Cancelled, processResult.Status);
+
+        }
+
+    }
+}
---
+++ tests/bdd/TransactionsStepDefinitions.cs
@@ -0,0 +1,154 @@
+using Bank4Us.Transaction;
+using NSubstitute;
+using Reqnroll;
+using System;
+
+namespace unit
+{
+    [Binding]
+    public class TransactionsStepDefinitions
+    {
+
+        private ScenarioContext _scenarioContext;
+
+        public TransactionsStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+
+        [Given("I want to deposit a positive amount into my account")]
+        public void GivenIWantToDepositAPositiveAmountIntoMyAccount()
+        {
+            int depositAmount = 1;
+            Transfer transferDeposit = new Transfer(TransferType.Deposit, new Account(), depositAmount);
+            ITransferWorkflow transferWorkflow = Substitute.For<ITransferWorkflow>();
+            transferWorkflow.Deposit(Arg.Any<Transfer>()).Returns(new TransferResult(TransferStatus.Approved, Array.Empty<TransferError>()));
+
+            _scenarioContext["transferWorkflow"] = transferWorkflow;
+            _scenarioContext["transferDeposit"] = transferDeposit;
+        }
+
+        [When("I make the deposit")]
+        public void WhenIMakeTheDeposit()
+        {
+            ITransferWorkflow transferWorkflow = (ITransferWorkflow)_scenarioContext["transferWorkflow"];
+            Transfer transferDeposit = (Transfer)_scenarioContext["transferDeposit"];
+
+            TransferAutomation transferAutomation = new TransferAutomation(transferWorkflow);
+            TransferResult transferResult = transferAutomation.InitiateTransfer(transferDeposit);
+
+            _scenarioContext["transferResult"] = transferResult;
+        }
+
+        [Then("The transfer should be approved")]
+        public void ThenTheTransferShouldBeApproved()
+        {
+            TransferResult transferResult = (TransferResult)_scenarioContext["transferResult"];
+
+            Assert.NotNull(transferResult);
+            Assert.Empty(transferResult.Errors);
+            Assert.Equal(TransferStatus.Approved, transferResult.Status);
+
+        }
+
+        [Given("I want to deposit a zero amount into my account")]
+        public void GivenIWantToDepositAZeroAmountIntoMyAccount()
+        {
+            int depositAmount = 0;
+            TransferError depositZeroError = new TransferError("Invalid deposit amount");
+            Transfer transferDeposit = new Transfer(TransferType.Deposit, new Account(), depositAmount);
+            ITransferWorkflow transferWorkflow = Substitute.For<ITransferWorkflow>();
+            transferWorkflow.Deposit(Arg.Any<Transfer>()).Returns(new TransferResult
+            (TransferStatus.Cancelled, new List<TransferError> { depositZeroError }));
+
+            _scenarioContext["depositZeroError"] = depositZeroError;
+            _scenarioContext["transferWorkflow"] = transferWorkflow;
+            _scenarioContext["transferDeposit"] = transferDeposit;
+        }
+
+        [Then("The transfer should be cancelled")]
+        public void ThenTheTransferShouldBeCancelled()
+        {
+            TransferResult transferResult = (TransferResult)_scenarioContext["transferResult"];
+            TransferError depositZeroError = (TransferError)_scenarioContext["depositZeroError"];
+
+            Assert.NotNull(transferResult);
+            Assert.NotEmpty(transferResult.Errors);
+            Assert.Equal(depositZeroError, transferResult.Errors[0]);
+            Assert.Equal(TransferStatus.Cancelled, transferResult.Status);
+
+
+        }
+
+        [Given("I have an account with Bank4Us with a balance of {int}")]
+        public void GivenIHaveAnAccountWithBank4UsWithABalanceOf(int balance)
+        {
+            _scenarioContext["account"] = new Account { Balance = balance };
+        }
+
+        [Given("I want to withdraw an amount less than or equal to my balance")]
+        public void GivenIWantToWithdrawAnAmountLessThanOrEqualToMyBalance()
+        {
+            Account account = (Account)_scenarioContext["account"];
+
+            Transfer transferWithdraw = new Transfer(TransferType.Withdraw, account, 200);
+            ITransferWorkflow transferWorkflow = Substitute.For<ITransferWorkflow>();
+            transferWorkflow.Withdraw(Arg.Any<Transfer>()).Returns(new TransferResult(TransferStatus.Approved, Array.Empty<TransferError>()));
+
+            _scenarioContext["transferWorkflow"] = transferWorkflow;
+            _scenarioContext["transferWithdraw"] = transferWithdraw;
+        }
+
+        [When("I make the withdrawl")]
+        public void WhenIMakeTheWithdrawl()
+        {
+            ITransferWorkflow transferWorkflow = (ITransferWorkflow)_scenarioContext["transferWorkflow"];
+            Transfer transferWithdraw = (Transfer)_scenarioContext["transferWithdraw"];
+
+            TransferAutomation transferAutomation = new TransferAutomation(transferWorkflow);
+            TransferResult transferResult = transferAutomation.InitiateTransfer(transferWithdraw);
+
+            _scenarioContext["transferResult"] = transferResult;
+        }
+
+        [Then("The withdrawl should be approved")]
+        public void ThenTheWithdrawlShouldBeApproved()
+        {
+            TransferResult transferResult = (TransferResult)_scenarioContext["transferResult"];
+
+            Assert.NotNull(transferResult);
+            Assert.Empty(transferResult.Errors);
+            Assert.Equal(TransferStatus.Approved, transferResult.Status);
+        }
+
+        [Given("I want to withdraw an amount greater than my balance")]
+        public void GivenIWantToWithdrawAnAmountGreaterThanMyBalance()
+        {
+            Account account = (Account)_scenarioContext["account"];
+            TransferError overdraftError = new TransferError("Invalid withdrawal amount, not enough funds");
+
+            Transfer transferWithdraw = new Transfer(TransferType.Withdraw, account, 201);
+            ITransferWorkflow transferWorkflow = Substitute.For<ITransferWorkflow>();
+            transferWorkflow.Withdraw(Arg.Any<Transfer>()).Returns(
+                new TransferResult(TransferStatus.Cancelled, new List<TransferError> { overdraftError }));
+
+            _scenarioContext["transferWorkflow"] = transferWorkflow;
+            _scenarioContext["transferWithdraw"] = transferWithdraw;
+            _scenarioContext["overdraftError"] = overdraftError;
+        }
+
+        [Then("The withdrawl should be cancelled")]
+        public void ThenTheWithdrawlShouldBeCancelled()
+        {
+            TransferResult transferResult = (TransferResult)_scenarioContext["transferResult"];
+            TransferError overdraftError = (TransferError)_scenarioContext["overdraftError"];
+
+            Assert.NotNull(transferResult);
+            Assert.NotEmpty(transferResult.Errors);
+            Assert.Equal(TransferStatus.Cancelled, transferResult.Status);
+        }
+
+
+    }
+}
